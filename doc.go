b434a89ac77b7b7fbc47cb4e602c2807d3f1a0a4
Damian Gryski <dgryski@gmail.com>
2016-11-28 15:52:02 +0100

    doc updates

--- doc.go
+++ doc.go
@@ -1,7 +1,13 @@
 // Package marvin32 implements Microsoft's InternalMarvin32HashString
 /*
 
-Based on the implementation at https://github.com/floodyberry/Marvin32
+This implementation is based on the reverse engineered version at
+
+    https://github.com/floodyberry/Marvin32
+
+Microsoft's reference implementation is now on GitHub under the MIT license
+
+    https://github.com/dotnet/coreclr/blob/master/src/vm/marvin32.cpp
 
 */
 package marvin32
--- marvin_stub.go
+++ marvin_stub.go
@@ -4,4 +4,5 @@ package marvin32
 
 //go:generate python -m peachpy.x86_64 sum.py -S -o marvin_amd64.s -mabi=goasm
 //go:noescape
+
 func Sum32(seed uint64, data []byte) uint32
