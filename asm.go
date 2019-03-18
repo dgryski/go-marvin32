571eeaf48c47c16f672d56dc82e343c8cf83508b
Damian Gryski <damian@gryski.com>
2019-03-17 20:55:45 -0700

    move from peachpy to avo

---
+++ asm.go
@@ -0,0 +1,109 @@
+// +build ignore
+
+package main
+
+import (
+	. "github.com/mmcloughlin/avo/build"
+	. "github.com/mmcloughlin/avo/operand"
+	. "github.com/mmcloughlin/avo/reg"
+)
+
+func update(lo, hi, v Register) {
+	ADDL(v, lo)
+	XORL(lo, hi)
+
+	ROLL(U8(20), lo)
+	ADDL(hi, lo)
+
+	ROLL(U8(9), hi)
+	XORL(lo, hi)
+
+	ROLL(U8(27), lo)
+	ADDL(hi, lo)
+
+	ROLL(U8(19), hi)
+}
+
+func main() {
+	Package("github.com/dgryski/go-marvin32")
+	TEXT("Sum32", NOSPLIT, "func(seed uint64, data []byte) uint32")
+	Doc("Sum32 computes the Marvin32 hash of the key")
+
+	reg_lo := GP32()
+	reg_hi := GP32()
+	reg_k1 := GP32()
+
+	t := GP64()
+	Load(Param("seed"), t)
+
+	MOVL(t.As32(), reg_lo)
+	SHRQ(U8(32), t)
+	MOVL(t.As32(), reg_hi)
+
+	reg_d := Load(Param("data").Base(), GP64())
+	reg_d_len := Load(Param("data").Len(), GP64())
+
+	loop_begin := "loop_begin"
+	loop_end := "loop_end"
+
+	CMPQ(reg_d_len, Imm(4))
+	JL(LabelRef(loop_end))
+	Label(loop_begin)
+	MOVL(Mem{Base: reg_d}, reg_k1)
+	update(reg_lo, reg_hi, reg_k1)
+	ADDQ(Imm(4), reg_d)
+	SUBQ(Imm(4), reg_d_len)
+	CMPQ(reg_d_len, U32(4))
+	JGE(LabelRef(loop_begin))
+	Label(loop_end)
+
+	MOVL(U32(0x80), reg_k1)
+
+	// no support for jump tables
+	after := "after"
+	sw3 := "sw3"
+	sw2 := "sw2"
+	sw1 := "sw1"
+
+	CMPQ(reg_d_len, Imm(0))
+	JE(LabelRef(after))
+
+	CMPQ(reg_d_len, Imm(0x03))
+	JE(LabelRef(sw3))
+
+	CMPQ(reg_d_len, Imm(0x02))
+	JE(LabelRef(sw2))
+
+	CMPQ(reg_d_len, Imm(0x01))
+	JE(LabelRef(sw1))
+
+	reg_b := GP32()
+
+	Label(sw3)
+	SHLL(U8(8), reg_k1)
+	MOVBLZX(Mem{Base: reg_d, Disp: 2}, reg_b)
+	ORL(reg_b, reg_k1)
+
+	Label(sw2)
+	SHLL(U8(8), reg_k1)
+	MOVBLZX(Mem{Base: reg_d, Disp: 1}, reg_b)
+	ORL(reg_b, reg_k1)
+
+	Label(sw1)
+	SHLL(U8(8), reg_k1)
+	MOVBLZX(Mem{Base: reg_d, Disp: 0}, reg_b)
+	ORL(reg_b, reg_k1)
+
+	Label(after)
+
+	update(reg_lo, reg_hi, reg_k1)
+	XORL(reg_k1, reg_k1)
+	update(reg_lo, reg_hi, reg_k1)
+
+	XORL(reg_hi, reg_lo)
+
+	Store(reg_lo, ReturnIndex(0))
+	RET()
+
+	Generate()
+}
--- marvin_stub.go
+++ marvin_stub.go
@@ -2,7 +2,7 @@
 
 package marvin32
 
-//go:generate python -m peachpy.x86_64 sum.py -S -o marvin_amd64.s -mabi=goasm
+//go:generate go run asm.go -out marvin_amd64.s
 //go:noescape
 
 func Sum32(seed uint64, data []byte) uint32
